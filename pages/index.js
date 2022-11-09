0e8fa820241d7252ddc9e05bdb676a6ed36e5969
Nathan Fox <neighfox@gmail.com>
2022-11-09 17:59:46 +0000

    Daisy Ui

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,92 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+
 import Typewriter from "typewriter-effect";
-import { Dropdown } from "flowbite-react";
-import { Navbar } from "flowbite-react";
 
 export default function Home() {
   return (
     <>
-      <Navbar fluid={true} rounded={true}>
-        <Navbar.Brand href="https://flowbite.com/">
-          <img
-            // src="https://flowbite.com/docs/images/logo.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt=""
-          />
-          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-            Fox Student
-          </span>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse>
-          <Navbar.Link href="/navbars" active={true}>
-            Home
-          </Navbar.Link>
-          <Navbar.Link href="/navbars">37</Navbar.Link>
-          <Navbar.Link href="/navbars">50</Navbar.Link>
-          <Navbar.Link href="/navbars">71</Navbar.Link>
-          <Navbar.Link href="/navbars">Contact</Navbar.Link>
-        </Navbar.Collapse>
-      </Navbar>
+      <div className="navbar">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h7"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>37</a>
+              </li>
+              <li>
+                <a>50</a>
+              </li>
+              <li>
+                <a>71</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <a className="btn btn-ghost normal-case text-xl">
+            <span className="lowercase text-base">Fox |</span>{" "}
+            <span className="  uppercase">| Student</span>
+          </a>
+        </div>
+        <div className="navbar-end">
+          <button className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          <button className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span className="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+        </div>
+      </div>
 
-      <div className="">
+      <div className="justify-center items-center text-center bg-slate-700 py-96 text-white  text-6xl">
         <Typewriter
           onInit={(typewriter) => {
             typewriter
@@ -41,6 +96,7 @@ export default function Home() {
               })
               .pauseFor(2500)
               .deleteAll()
+              .typeString("Fox Student lets is under construction!!")
               .callFunction(() => {
                 console.log("All strings were deleted");
               })
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/flowbite-react/**/*.js",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["cupcake"],
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("daisyui")],
 };
